baec488a0d70bbac9d70965cc49b248b619afded
Kapusniak <goldkapusniak@proton.me>
2023-08-17 12:29:43 +0200

    feat: include claim delay of 2d, avoid arbs

--- src/PermaBurner.sol
+++ src/PermaBurner.sol
@@ -6,12 +6,18 @@ import {IBpt} from "./interfaces/IBpt.sol";
 import {IBalancerVault} from "./interfaces/IBalancerVault.sol";
 
 contract PermaBurner {
+    struct Claim {
+        uint256 claimableGoldAmount;
+        uint256 claimableTs;
+    }
+
     IERC20 public constant GOLD =
         IERC20(0xbeFD5C25A59ef2C1316c5A4944931171F30Cd3E4);
 
     uint256 public constant BPT_MULTIPLIER = 2;
     uint256 public constant BASE = 10_000;
     uint256 public constant PREMIUM = 10_300;
+    uint256 public constant CLAIM_DELAY = 2 days;
 
     IBalancerVault public constant BALANCER_VAULT =
         IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
@@ -23,15 +29,15 @@ contract PermaBurner {
     address public constant BURN_DESTINATION =
         0xbeFD5C25A59ef2C1316c5A4944931171F30Cd3E4;
 
+    // address (bpt depositor) => Claim
+    mapping(address => Claim) public claimDetails;
+
     error ZeroBpt();
     error NotEOA(address caller);
+    error TooEarlyGoldenBoy(uint256 currentTime, uint256 claimableTime);
 
-    event Deposit(
-        address depositor,
-        uint256 bptAmount,
-        uint256 goldRewarded,
-        uint256 timestamp
-    );
+    event Deposit(address depositor, uint256 bptAmount, uint256 timestamp);
+    event Claimed(address claimer, uint256 goldRewarded, uint256 timestamp);
 
     /// @notice Deposits BPT and burns GOLD
     /// @param _amount Amount of BPT to deposit
@@ -44,13 +50,37 @@ contract PermaBurner {
 
         uint256 goldReward = _getGoldRewardOut(_amount);
 
-        // rewards to depositor
-        GOLD.transfer(msg.sender, goldReward);
+        claimDetails[msg.sender] = Claim({
+            claimableGoldAmount: goldReward,
+            claimableTs: block.timestamp + CLAIM_DELAY
+        });
 
-        // burn gold
-        GOLD.transfer(BURN_DESTINATION, goldReward);
+        emit Deposit(msg.sender, _amount, block.timestamp);
+    }
 
-        emit Deposit(msg.sender, _amount, goldReward, block.timestamp);
+    /// @notice Allows bpt golden boyz depositor after the delay to claim his/her entitled scarce amount of $GOLD!
+    function claim() external {
+        Claim memory cachedClaimDets = claimDetails[msg.sender];
+
+        if (block.timestamp < cachedClaimDets.claimableTs)
+            revert TooEarlyGoldenBoy(block.timestamp, cachedClaimDets.claimableTs);
+
+        claimDetails[msg.sender] = Claim({
+            claimableGoldAmount: 0,
+            claimableTs: 0
+        });
+
+        // rewards to depositor life changing $GOLD tokens!
+        GOLD.transfer(msg.sender, cachedClaimDets.claimableGoldAmount);
+
+        // burn gold forever, total supply reduction!
+        GOLD.transfer(BURN_DESTINATION, cachedClaimDets.claimableGoldAmount);
+
+        emit Claimed(
+            msg.sender,
+            cachedClaimDets.claimableGoldAmount,
+            block.timestamp
+        );
     }
 
     /// @notice Calculates the amount of GOLD to reward for a given BPT amount
